3117848befd0e61a2b5438cf9e6f7bda7b5a0ae1
Aituar Semeibek <113921403+LeonRusiko@users.noreply.github.com>
2023-05-17 15:09:35 +0600

    Add files via upload

---
+++ lesson9/src/App/App.tsx
@@ -0,0 +1,13 @@
+import MainPage from "../Pages/MainPage";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div>
+      <h2>hw 9</h2>
+      <MainPage />
+    </div>
+  );
+};
+
+export default App;
